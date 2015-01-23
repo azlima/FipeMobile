8e9f758370e428394748fe6a91c5514150c23083
azlima <gilbertoluis@terra.com.br>
2015-01-23 17:52:00 -0200

    Mobile version

--- FipeMobile/Global.asax.cs
+++ FipeMobile/Global.asax.cs
@@ -22,6 +22,7 @@ namespace FipeMobile
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleMobileConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
