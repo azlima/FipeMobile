9017af7aa7661ec9d808bba6798cd8e64b3954b0
azlima <gilbertoluis@terra.com.br>
2015-01-26 17:29:50 -0200

    Test wp mobile.

--- FipeMobile/Controllers/VeiculoController.cs
+++ FipeMobile/Controllers/VeiculoController.cs
@@ -5,6 +5,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Web.Mvc;
@@ -19,47 +20,59 @@
 
             Session["marca"] = new Marca() { id = marcaId, name = marcaName, tipo = (TipoVeiculo)tipo, fipe_name = fipeName };
 
-            //Classe de suporte a consumo de HTTP namespace System.Net.Http
-            HttpClient client = new HttpClient();
-            //Adicionando no Header o token e o media type JSON
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            string uri = string.Format("http://fipeapi.appspot.com/api/1/{1}/veiculos/{0}.json", marcaId.ToString(), Enum.GetName(typeof(TipoVeiculo), (tipo)).ToLower());
-            //Fazendo a requisição
-            HttpResponseMessage response = client.GetAsync(uri).Result;
-            //Conferindo código 200 de sucesso
-            if (response.IsSuccessStatusCode)
+            if (Request.Browser.IsMobileDevice)
             {
-                try
+                //WebClient webClient = new WebClient();
+
+                //string webUri = string.Format("http://fipeapi.appspot.com/api/1/{1}/veiculos/{0}.json", marcaId.ToString(), Enum.GetName(typeof(TipoVeiculo), (tipo)).ToLower());
+
+                //Uri nu = new Uri(webUri);
+                //webClient.DownloadStringAsync(nu);
+            }
+            else
+            {
+                //Classe de suporte a consumo de HTTP namespace System.Net.Http
+                HttpClient client = new HttpClient();
+                //Adicionando no Header o token e o media type JSON
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string uri = string.Format("http://fipeapi.appspot.com/api/1/{1}/veiculos/{0}.json", marcaId.ToString(), Enum.GetName(typeof(TipoVeiculo), (tipo)).ToLower());
+                //Fazendo a requisição
+                HttpResponseMessage response = client.GetAsync(uri).Result;
+                //Conferindo código 200 de sucesso
+                if (response.IsSuccessStatusCode)
                 {
-                    //Parse de JSON para o objeto Veiculos
-                    var resultsCount = response.Content.ReadAsAsync<object[]>().Result.Count();
-                    if (resultsCount > 1)
+                    try
                     {
-                        foreach (var veiculo in client.GetAsync(uri).Result.Content.ReadAsAsync<IList<Veiculo>>().Result)
+                        //Parse de JSON para o objeto Veiculos
+                        var resultsCount = response.Content.ReadAsAsync<object[]>().Result.Count();
+                        if (resultsCount > 1)
                         {
+                            foreach (var veiculo in client.GetAsync(uri).Result.Content.ReadAsAsync<IList<Veiculo>>().Result)
+                            {
+                                veiculo.Marca = (Marca)Session["marca"];
+                                veiculos.Add(veiculo);
+                            }
+                        }
+                        else if (resultsCount == 1)
+                        {
+                            var veiculo = client.GetAsync(uri).Result.Content.ReadAsAsync<Veiculo>().Result;
                             veiculo.Marca = (Marca)Session["marca"];
                             veiculos.Add(veiculo);
                         }
                     }
-                    else if (resultsCount == 1)
+                    catch //(AggregateException aggregateException)
                     {
-                        var veiculo = client.GetAsync(uri).Result.Content.ReadAsAsync<Veiculo>().Result;
-                        veiculo.Marca = (Marca)Session["marca"];
-                        veiculos.Add(veiculo);
+                        return View(veiculos);
+                        //throw new Exception(ae.Message);
                     }
                 }
-                catch //(AggregateException aggregateException)
+                else
                 {
+                    //Exibindo a exceção com o código Http respectivo.
+                    //throw new Exception(string.Concat(response.StatusCode.ToString(), " - ", response.ReasonPhrase));
                     return View(veiculos);
-                    //throw new Exception(ae.Message);
                 }
-            }
-            else
-            {
-                //Exibindo a exceção com o código Http respectivo.
-                //throw new Exception(string.Concat(response.StatusCode.ToString(), " - ", response.ReasonPhrase));
-                return View(veiculos);
             }
 
             return View(veiculos);
